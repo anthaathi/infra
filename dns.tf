34c40e63e6ecd236b77c341ab10e6163e0883e22
Omkar Yadav <me@f22.dev>
2022-02-16 20:03:25 +0400

    removed dns

--- dns.tf
+++
@@ -1,9 +0,0 @@
-resource "google_dns_managed_zone" "primary_zone" {
-  name        = "${var.prefix}-${replace(var.domain, ".", "-")}-zone"
-  dns_name    = "${var.domain}."
-  description = "Domain"
-  visibility = "public"
-  labels = {
-    app = var.prefix
-  }
-}
