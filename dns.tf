0aee3a1ba7fae87b7def6692f54b4fc88583c238
Omkar Yadav <omkar@anthaathi.in>
2022-02-16 18:26:06 +0400

    initial commit

---
+++ dns.tf
@@ -0,0 +1,9 @@
+resource "google_dns_managed_zone" "primary_zone" {
+  name        = "${var.prefix}-${var.domain}-zone"
+  dns_name    = "${var.domain}."
+  description = "Domain"
+  visibility = "public"
+  labels = {
+    app = var.prefix
+  }
+}
